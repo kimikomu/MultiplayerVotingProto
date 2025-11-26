7a10a86972108d2363a98e5ca81b9f3d20645208
Kimiko Muwanguzi <kimikomu@gmail.com>
2025-11-26 00:13:27 -0500

    Combine game state enum script and event args class script into 1 StateMachine script

--- Assets/Scripts/Core/StateMachine.cs
+++ Assets/Scripts/Core/StateMachine.cs
@@ -2,6 +2,16 @@ using System;
 
 namespace Core
 {
+    public enum GameState
+    {
+        Lobby,
+        Prompt,
+        Submit,
+        Vote,
+        Reveal,
+        GameOver
+    }
+    
     public class GameStateEventArgs : EventArgs
     {
         public GameState PreviousState { get; set; }
